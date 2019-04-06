a6311e2749bd835430229ed1d090c0e83cacb9f2
August Boza <abozadev@gmail.com>
2019-04-06 23:56:01 +0200

    Add get user info request

--- twitter.js
+++ twitter.js
@@ -10,6 +10,7 @@ var client = new Twitter({
   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
 
+
 var getSearch = function(params, callback)
 {
   client.get('search/tweets', params, function(error, tweets, response) {
@@ -28,5 +29,23 @@ var getUserTweets = function(params, callback)
   });
 }
 
+var getUserInfo = function(params, callback)
+{
+  console.log(params.token);
+  var clientUser = new Twitter({
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    bearer_token: params.token
+  });
+
+  clientUser.get('users/show', {screen_name: params.screen_name}, function(error, tweets, response) {
+    if (!error) {
+      //console.log(tweets);
+      callback.send(tweets);
+    }
+  })
+}
+
 module.exports.getSearch = getSearch;
 module.exports.getUserTweets = getUserTweets;
+module.exports.getUserInfo = getUserInfo;
