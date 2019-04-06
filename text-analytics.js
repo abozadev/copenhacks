6f0d2e35a8b127d05f92e31ffb20dc0ee0531934
eloimuns <eloimuns@gmail.com>
2019-04-06 19:19:41 +0200

    fixes

--- text-analytics.js
+++ text-analytics.js
@@ -46,40 +46,44 @@ let parseSentimentData = function(body, listTweets, callback){
 }
 
 let getSentimentsFromTwitterData = function(twitterData, callback){
-  if (twitterData != null && twitterData != undefined && twitterData.length > 0){
+  if (twitterData != null && twitterData != undefined ){
     if (twitterData.statuses !== null && twitterData.statuses !== undefined){
       twitterData = twitterData.statuses;
     }
 
-    let listTweets = {'documents' : []};
-    var index = 0;
-
-    twitterData.forEach(function(tweet){
-      if (tweet.retweeted_status == undefined){
-        listTweets.documents.push({
-          'id' : index,
-          'text': tweet.text,
-          'language' : tweet.lang
-        });
-      }
-      index++;
-    });
-
-    if (listTweets.documents.length > 0){
-      let body = '';
-      let req = https.request (getRequestParams(endpoints.SENTIMENT), (res) => {
-        res.on ('data', function (d) {
-          body += d;
-        });
-        res.on ('end', function () {
-          parseSentimentData(JSON.parse(body), twitterData, callback);
-        });
+    if (twitterData.length > 0){
+      let listTweets = {'documents' : []};
+      var index = 0;
+  
+      twitterData.forEach(function(tweet){
+        if (tweet.retweeted_status == undefined){
+          listTweets.documents.push({
+            'id' : index,
+            'text': tweet.text,
+            'language' : tweet.lang
+          });
+        }
+        index++;
       });
-      req.write(JSON.stringify(listTweets));
-      req.end();
+  
+      if (listTweets.documents.length > 0){
+        let body = '';
+        let req = https.request (getRequestParams(endpoints.SENTIMENT), (res) => {
+          res.on ('data', function (d) {
+            body += d;
+          });
+          res.on ('end', function () {
+            parseSentimentData(JSON.parse(body), twitterData, callback);
+          });
+        });
+        req.write(JSON.stringify(listTweets));
+        req.end();
+      } else {
+        callback.send("NOT ENOUGH TWEETS")
+      }
     } else {
       callback.send("NOT ENOUGH TWEETS")
-    }
+    }   
 
   } else {
     callback.send("ERROR");
