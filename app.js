b4447ffc8329f2189a3331877037fad4ee37c952
August Boza <abozadev@gmail.com>
2019-04-06 14:54:41 +0200

    Inital Config

--- app.js
+++ app.js
@@ -1,12 +1,35 @@
 const http = require('http');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const hostname = '127.0.0.1';
 const port = 3000;
+
+var Twitter = require('twitter');
+
+var client = new Twitter({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+});
+
+var params = {screen_name: 'nodejs'};
+client.get('statuses/user_timeline', params, function(error, tweets, response) {
+  if (!error) {
+    console.log(tweets);
+  }
+});
+
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World\n');
+  client.get('search/tweets', {q: 'node.js'}, function(error, tweets, response) {
+   console.log(tweets);
+  });
 });
 
 server.listen(port, hostname, () => {
