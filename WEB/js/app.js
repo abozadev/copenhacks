63c66095e83b01461b1133594e5a2b4bbc5c954a
eloimuns <eloimuns@gmail.com>
2019-04-07 07:11:58 +0200

    AAA

--- WEB/js/app.js
+++ WEB/js/app.js
@@ -1,1 +1,9 @@
 var app = angular.module('web', ['ngRoute', 'chart.js', 'ngTagsInput', 'moment-picker']);
+
+
+app.config(['ChartJsProvider', function (ChartJsProvider) {
+// Configure all charts
+ChartJsProvider.setOptions({
+    responsive: false
+});
+}])
